d6e35942d2a92726d48781e21707e0e981c8a195
Gabriel <shadoways123@gmail.com>
2016-03-16 16:56:35 -0300

    apagando teste adicionado ao arquivo

--- index.php
+++ index.php
@@ -18,7 +18,6 @@
  * @link          http://cakephp.org CakePHP(tm) Project
  * @since         CakePHP(tm) v 0.2.9
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
- * @teste         docker
  */
 
 /**
